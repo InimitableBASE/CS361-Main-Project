cfe62109b2ad1c565581db131d77774a976e572b
Justin Holley <jholley6@gmail.com>
2025-10-18 08:54:55 -0600

    Added Basic Main Menu and Clock in Menu

---
+++ timekeeper.py
@@ -0,0 +1,40 @@
+# Author Justin Holley
+# CS361 - Fall 2025
+
+def main_menu():
+    print("\t\t\t\tWELCOME TO TIMEKEEPER!")
+    print("\t\tThe Program that tracks your work hours so you can get paid!\n")
+    print("MAIN MENU\n")
+    print("Select a numerical option from the list below by pressing that "
+          "numbers respective key on the keyboard:")
+    print("(1)\tClock In")
+    print("(2)\tClock Out")
+    print("(3)\tSupervisor Menu")
+    print("(4)\tQuit")
+    print("\n\"Clock In\" tracks when you start work, \"Clock out\" tracks "
+          "when you stop work. Each takes less than a minute to help ensure "
+          "you are paid for your hard work!")
+    print("\nNot sure if you're clocked in or out? Don't worry! You can try "
+          "clocking in or clocking out again and the program will tell you if "
+          "you are already clocked in or out!")
+
+def clock_in():
+    print("CLOCK IN\n")
+    print("Clocking in tracks when you start work.\n")
+    print("INSTRUCTIONS:")
+    print("To Clock in for work, please enter your Employee ID and Press "
+          "Enter.")
+    print("To return to the Main Menu, press the Escape (Esc.")
+
+    print("(1)\tClock In")
+    print("(2)\tClock Out")
+    print("(3)\tSupervisor Menu")
+    print("(4)\tQuit")
+
+
+
+def main():
+    main_menu()
+
+if __name__ == "__main__":
+    main()
