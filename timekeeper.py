3fef1fd71fdfb91de1cb4952e2fa0762c593cf06
Justin Holley <jholley6@gmail.com>
2025-10-18 20:16:14 -0600

    Added Clocked Out Screen and helper functions it needs.

--- timekeeper.py
+++ timekeeper.py
@@ -108,21 +108,46 @@ def _formatDate(time):
     return time.strftime('%m/%d/%Y')
 
 def _new_clock_in(entry):
-        f_name = Employees[entry]['first']
-        l_name = Employees[entry]['last']
-        time = datetime.now()
-        card = {
-                'cit': time, 
-                'cot': "",
-                'hrs': ""
-                }
-        Employees[entry]['time_cards'].append(card)
-        Employees[entry]['clocked_in'] = True
-        Employees[entry]['last_clock_in'] = time
-        date = _formatDate(time)
-        time = _formatTime(time)
-        # time = Employees[Cur_Entry]['time_cards'][-1]['cit']
-        print(f'\n{f_name} {l_name} HAS CLOCKED IN ON {date} AT {time}')
+    f_name = Employees[entry]['first'].upper()
+    l_name = Employees[entry]['last'].upper()
+    time = datetime.now()
+    card = {
+            'cit': time, 
+            'cot': "",
+            'hrs': ""
+            }
+    Employees[entry]['time_cards'].append(card)
+    Employees[entry]['clocked_in'] = True
+    Employees[entry]['last_clock_in'] = time
+    date = _formatDate(time)
+    time = _formatTime(time)
+    # time = Employees[Cur_Entry]['time_cards'][-1]['cit']
+    print(f'\n{f_name} {l_name} HAS CLOCKED IN ON {date} AT {time}')
+
+def _new_clock_out(entry):
+    time = datetime.now()        
+    f_name = Employees[entry]['first'].upper()
+    l_name = Employees[entry]['last'].upper()
+    Employees[entry]['time_cards'][-1]['cot'] = time
+    Employees[entry]['clocked_in'] = False
+    work_duration = time - Employees[entry]['time_cards'][-1]['cit']
+    sec_worked = work_duration.total_seconds()
+    min_worked = sec_worked / 60
+    hrs_worked = sec_worked / 3600
+    Employees[entry]['time_cards'][-1]['hrs'] = hrs_worked
+    date = _formatDate(time)
+    time = _formatTime(time)
+    if hrs_worked < 1:
+        print(
+            f"\n{f_name} {l_name} WORKED {min_worked:.1f} MINUTES AND CLOCKED "
+            f"OUT ON {date} AT {time}"
+            )   
+    else:
+        print(
+            f"\n{f_name} {l_name} WORKED {hrs_worked} HOURS AND CLOCKED OUT ON"
+            f" {date} AT {time}"
+            )
+
 
 """Main Menu Screen"""
 def main_menu():
@@ -145,9 +170,12 @@ def main_menu():
     while(True):
         Cur_Entry = getchar()
         if Cur_Entry == "1":
-            return "clock_in"       
+            return "clock_in"
+        if Cur_Entry == "2":
+            return "clock_out"       
         if Cur_Entry == "4":
             return "_quit_"
+        
     
 
 
@@ -162,9 +190,8 @@ def clock_in():
     print("To clock in for work, please enter your Employee ID and press "
           "Enter.")
     print("To return to the Main Menu, press the Escape key (Esc).")
-    print("\nNot sure if you're clocked in or out? Don't worry! You can try "
-        "clocking in or clocking out again and the program will tell you if "
-        "you are already clocked in or out!")
+    print("\nNot sure if you're clocked in? Don't worry! You can try clocking "
+          "in and the program will tell you if you are already clocked in!")
     
     print("\nPlease enter your Employee ID: ", end="", flush=True)
     
@@ -172,9 +199,9 @@ def clock_in():
 
     # User Exists and is already clocked in
     if Cur_Entry in Employees and Employees[Cur_Entry]["clocked_in"]:
-        f_name = Employees[Cur_Entry]['first']
-        l_name = Employees[Cur_Entry]['last']
-        print(f'{f_name} {l_name} is already clocked in!')
+        f_name = Employees[Cur_Entry]['first'].upper()
+        l_name = Employees[Cur_Entry]['last'].upper()
+        print(f'\n{f_name} {l_name} IS ALREADY CLOCKED IN!')
         print("\nPress the Escape key (Esc) to return to the Main Menu, or "
               "any other key to try again.")
         ch = getchar()
@@ -188,8 +215,6 @@ def clock_in():
         print("\nPress any key to return to the Main Menu.")
         getchar()
         return "main_menu"
-        # TODO: Add users clock in to time card
-       
 
     # User pressed Escape Key, go back to Main menu
     if Cur_Entry == None:
@@ -206,9 +231,65 @@ def clock_in():
             return "main_menu"
         return "clock_in"    
 
+""" Clock Out Screen """
+def clock_out():
+    os.system('cls')    # Clear Screen
+    Cur_Entry = ''   # Clear Cur_Entry for use in this screen.  
+    
+    print("CLOCK OUT\n")
+    print("Clocking out tracks when you stop work.\n")
+    print("INSTRUCTIONS:")
+    print("To clock out from work, please enter your Employee ID and press "
+          "Enter.")
+    print("To return to the Main Menu, press the Escape key (Esc).")
+    print("\nNot sure if you're clocked out? Don't worry! You can try clocking"
+          " out again and the program will tell you if you are already "
+          "clocked out!")
+    
+    print("\nPlease enter your Employee ID: ", end="", flush=True)
+    
+    Cur_Entry = getentry()
+
+    # User Exists and is already clocked out
+    if Cur_Entry in Employees and not Employees[Cur_Entry]["clocked_in"]:
+        f_name = Employees[Cur_Entry]['first'].upper()
+        l_name = Employees[Cur_Entry]['last'].upper()
+        print(f'\n{f_name} {l_name} IS ALREADY CLOCKED OUT!') 
+        print("\nYou must clock in before you may clock out. If you forgot to "
+              "clock in for work, see a Supervisor.")
+        print("\nPress the Escape key (Esc) to return to the Main Menu, or "
+              "any other key to try again.")
+        ch = getchar()
+        if ch == '\x1b':
+            return "main_menu"
+        return "clock_out"
+    
+    # User Exists & Not already Clocked in - Clock them in
+    elif Cur_Entry in Employees:
+        _new_clock_out(Cur_Entry)
+        print("\nPress any key to return to the Main Menu.")
+        getchar()
+        return "main_menu"
+
+    # User pressed Escape Key, go back to Main menu
+    if Cur_Entry == None:
+        return "main_menu"
+    
+    # Invalid Entry
+    if Cur_Entry not in Employees:
+        print("\nThat is not a valid Employee ID. See a Supervisor "
+              "if you have forgotten your Employee ID.")
+        print("\nPress the Escape key (Esc) to return to the Main Menu, or "
+              "any other key to try again.")
+        ch = getchar()
+        if ch == '\x1b':
+            return "main_menu"
+        return "clock_out"    
+
 SCREENS = {
     "main_menu": main_menu,
-    "clock_in": clock_in
+    "clock_in": clock_in,
+    "clock_out": clock_out
 }
 
 def main():
